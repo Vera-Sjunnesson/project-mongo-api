f9db411ca6a4482fa342eb34393c9ce159796fa9
Vera Sjunnesson <verasjunnesson@MacBook-Air-som-tillhor-Ulla-4.local>
2023-05-09 23:10:06 +0200

    setting up project

--- server.js
+++ server.js
@@ -1,14 +1,9 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import dotenv from 'dotenv'
 
-// If you're using one of our datasets, uncomment the appropriate import below
-// to get started!
-// import avocadoSalesData from "./data/avocado-sales.json";
-// import booksData from "./data/books.json";
-// import goldenGlobesData from "./data/golden-globes.json";
-// import netflixData from "./data/netflix-titles.json";
-// import topMusicData from "./data/top-music.json";
+dotenv.config()
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/project-mongo";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -23,10 +18,77 @@ const app = express();
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
+const listEndPoints = require('express-list-endpoints');
+
+app.use((req, res, next) => {
+  if (mongoose.connection.readyState === 1) {
+    next()
+  } else {
+    res.status(503).json({ error: "Service unavailable"})
+  }
+}
+)
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.send("Hello Technigo!");
+  res.send(process.env.API_KEY)
+  res.json(listEndPoints(app));
+});
+
+const { Schema } = mongoose;
+const userSchema = new Schema ({
+  name: String,
+  age: Number,
+  alive: Boolean
+});
+
+const User = mongoose.model("User", userSchema);
+
+const songSchema = new Schema({
+  id: Number,
+  trackName: String,
+  artistName: String,
+  genre: String,
+  bpm: Number,
+  energy: Number,
+  danceability: Number,
+  loudness: Number,
+  liveness: Number,
+  valence: Number,
+  length: Number,
+  acousticness: Number,
+  speechiness: Number,
+  popularity: Number
+})
+
+const Song = mongoose.model("Song", songSchema);
+
+
+
+app.get("/songs/id/:id", async (req, res) => {
+try {
+  const singleSong = await Song.findById(req.params.id);
+  if (singleSong) {
+    res.status(200).json({
+      success: true,
+      body: singleSong
+    })
+  } else {
+    res.status(404).json({
+      success: false,
+      body: {
+        message: "Song not found"
+      }
+    })
+  }
+} catch(err) {
+  res.status(500).json({
+    success: false,
+    body: {
+      message: "invalid song id"
+    }
+  })
+}
 });
 
 // Start the server

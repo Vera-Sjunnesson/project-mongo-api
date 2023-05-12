3a11b99851ca8a8555bce968cc7e8cc0767bff8e
Vera Sjunnesson <verasjunnesson@MacBook-Air-som-tillhor-Ulla-4.local>
2023-05-12 20:50:26 +0200

    continued working on endpoints

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 
+import a24MoviesList from './data/a24movies.json';
+
 dotenv.config()
 
 const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/project-mongo'
@@ -20,6 +22,54 @@ app.use(cors());
 app.use(express.json());
 const listEndPoints = require('express-list-endpoints');
 
+const { Schema } = mongoose;
+
+/* const userSchema = new Schema ({
+  name: String,
+  age: Number,
+  alive: Boolean
+});
+
+const User = mongoose.model("User", userSchema);
+ */
+const a24MoviesSchema = new Schema({
+  id: Number,
+  title: String,
+  Directed_by: [String],
+  Written_by: [String],
+  Produced_by: [String],
+  Starring: [String],
+  Cinematography: [String],
+  Edited_by: [String],
+  Music_by: [String],
+  Production_companies: [String],
+  Distributed_by: [String],
+  Country: String,
+  Language: String,
+  Running_time: Number,
+  Budget: Number,
+  Box_office: Number,
+  Release_dates: String,
+  imdb_score: Number,
+  metascore: Number,
+  rotten_tomatoes: String,
+  Screenplay_by: [String],
+  Based_on: [String]
+})
+
+const A24Movies = mongoose.model("A24Movies", a24MoviesSchema);
+
+if(process.env.RESET_DB) {
+  const resetDatabase = async () => {
+    await A24Movies.deleteMany()
+    a24MoviesList.forEach((singleMovie) => {
+      const newMovie = new A24Movies(singleMovie);
+      newMovie.save();
+    })
+  }
+  resetDatabase();
+}
+
 app.use((req, res, next) => {
   if (mongoose.connection.readyState === 1) {
     next()
@@ -35,47 +85,42 @@ app.get("/", (req, res) => {
   res.json(listEndPoints(app));
 });
 
-const { Schema } = mongoose;
-const userSchema = new Schema ({
-  name: String,
-  age: Number,
-  alive: Boolean
-});
+app.get("/a24movies", async (req, res) => {
+  const { director, imdb } = req.query;
+  const response = {
+    success: true,
+    body: {}
+  }
+  const directorRegex = new RegExp(director, 'i')
+  const imdbScorequery = { $gt: imdb ? imdb : 0 }
+  try {
+    const searchResultFromDB = await A24Movies.find({ Directed_by: { $all: [directorRegex] }, imdb_score: imdbScorequery })
+    if (searchResultFromDB.length > 0) {
+      response.body = searchResultFromDB
+      res.status(200).json(response)
+    } else {
+      response.success = false,
+      res.status(500).json(response)
+    }
+  } catch(err) {
+    response.success = false,
+    res.status(500).json(response)
+  }
+  });
 
-const User = mongoose.model("User", userSchema);
-
-const songSchema = new Schema({
-  id: Number,
-  trackName: String,
-  artistName: String,
-  genre: String,
-  bpm: Number,
-  energy: Number,
-  danceability: Number,
-  loudness: Number,
-  liveness: Number,
-  valence: Number,
-  length: Number,
-  acousticness: Number,
-  speechiness: Number,
-  popularity: Number
-})
-
-const Song = mongoose.model("Song", songSchema);
-
-app.get("/songs/id/:id", async (req, res) => {
+app.get("/a24movies/id/:id", async (req, res) => {
 try {
-  const singleSong = await Song.findById(req.params.id);
-  if (singleSong) {
+  const singleMovie = await A24Movies.findById(req.params.id);
+  if (singleMovie) {
     res.status(200).json({
       success: true,
-      body: singleSong
+      body: singleMovie
     })
   } else {
     res.status(404).json({
       success: false,
       body: {
-        message: "Song not found"
+        message: "Movie not found"
       }
     })
   }
@@ -83,7 +128,7 @@ try {
   res.status(500).json({
     success: false,
     body: {
-      message: "invalid song id"
+      message: "Invalid movie id"
     }
   })
 }

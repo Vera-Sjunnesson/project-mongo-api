837b1f2e6bae0555246e6988334c95d7830ca476
Vera Sjunnesson <verasjunnesson@MacBook-Air-som-tillhor-Ulla-4.local>
2023-05-14 17:42:07 +0200

    worked on endpoints

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ mongoose.Promise = Promise;
 const port = process.env.PORT || 8080;
 const app = express();
 
+// Swagger for API documentation
+
 // Add middlewares to enable cors and json body parsing
 app.use(cors());
 app.use(express.json());
@@ -51,6 +53,7 @@ const a24MoviesSchema = new Schema({
 
 const A24Movies = mongoose.model("A24Movies", a24MoviesSchema);
 
+
 if(process.env.RESET_DB) {
   const resetDatabase = async () => {
     await A24Movies.deleteMany()
@@ -73,29 +76,243 @@ app.use((req, res, next) => {
 
 // Start defining your routes here
 app.get("/", (req, res) => {
-  res.json(listEndPoints(app));
+
+  const welcomeText = "Top movies from A24 Studio, one of the best movie studios in the world";
+  const apiDocumentation = "https://project-mongo-api-ozexcouyaq-lz.a.run.app";
+  const endpoints = (listEndPoints(app))
+
+  res.send({
+    body: {
+      welcomeText,
+      apiDocumentation,
+      endpoints
+    }
+  });
 });
 
-app.get("/a24movies", async (req, res) => {
-  const { director, imdb } = req.query;
-  const response = {
-    success: true,
-    body: {}
-  }
+//Get all songs in the dataset with paging, params are page and limit
+app.get("/movies", async (req, res) => {
+  // destructure page and limit and set default values
+  const { page = 1, limit = 10, director, imdb, producer, starring } = req.query;
+
   const directorRegex = new RegExp(director, 'i')
+  const producerRegex = new RegExp(producer, 'i')
+  const starringRegex = new RegExp(starring, 'i')
   const imdbScorequery = { $gt: imdb ? imdb : 0 }
   try {
-    const searchResultFromDB = await A24Movies.find({ Directed_by: { $all: [directorRegex] }, imdb_score: imdbScorequery })
-    if (searchResultFromDB.length > 0) {
-      response.body = searchResultFromDB
-      res.status(200).json(response)
+
+    // execute query with page and limit values
+    const movieList = await A24Movies.find({ Directed_by: { $all: [directorRegex] }, Produced_by: { $all: [producerRegex] }, Starring: { $all: [starringRegex] }, imdb_score: imdbScorequery })
+      .limit(limit * 1)
+      .skip((page - 1) * limit)
+      .exec();
+
+    // get total documents in the Posts collection
+    const count = await A24Movies.countDocuments();
+
+     // return response with posts, total pages, and current page
+    if (movieList.length > 0) {
+      res.status(200).json({
+        success: true,
+        body: {
+          movieList,
+          totalPages: Math.ceil(count / limit),
+          currentPage: page,
+          totalMovies: count
+        }
+      });
     } else {
-      response.success = false,
-      res.status(500).json(response)
+      res.status(404).json({
+        success: false,
+        body: {
+          message: "No movies found in list"
+        }
+      })
     }
   } catch(err) {
-    response.success = false,
-    res.status(500).json(response)
+    res.status(500).json({
+      success: false,
+      body: {
+        message: "Invalid movie list"
+      }
+    })
+  }
+});
+
+app.get("/movies/score", async (req, res) => {
+  const { imdb, metascore, rottentomatoes } = req.query;
+  const imdbScorequery = { $gt: imdb ? imdb : 0 }
+  const metaScorequery = { $gt: metascore ? metascore : 0 }
+  
+  const pipeline = [
+    {
+      $match: {
+        imdb_score: imdbScorequery,
+        metascore: metaScorequery,
+        rotten_tomatoes: { $regex: `^${rottentomatoes}` }
+      }
+    },
+    {
+      $addFields: {
+        rotten_tomatoes_number: {
+          $convert: { input: { $trim: { input: "$rotten_tomatoes", chars: "%" } }, to: "double" }
+        }
+      }
+    },
+    { $sort: { rotten_tomatoes_number: -1, imdb_score: -1, metascore: -1 } },
+    { $limit: 5 }
+  ];
+
+  try {
+    const scoredMovies = await A24Movies.aggregate(pipeline);
+    const top5ImdbMovies = await A24Movies.find({}).sort({imdb_score: -1, metascore: -1 }).limit( 5 )
+    if (scoredMovies.length > 0) {
+      res.status(200).json({
+        success: true,
+        body: scoredMovies
+      })
+    } else if (top5ImdbMovies.length > 0) {
+      res.status(200).json({
+        success: true,
+        body: top5ImdbMovies
+      })
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: "Score list not found"
+        }
+      })
+    }
+  } catch(err) {
+    res.status(500).json({
+      success: false,
+      body: {
+        message: "Invalid score list"
+      }
+    })
+  }
+  });
+
+
+
+// Get movies by title
+app.get("/movies/:title", async (req, res) => {
+  const singleTitle = req.params.title
+  const { director } = req.query;
+  
+  const titleRegex = new RegExp(singleTitle, 'i')
+  const directorRegex = new RegExp(director, 'i')
+  try {
+    const searchedTitle = await A24Movies.find({ title: titleRegex, Directed_by: { $all: [directorRegex] } })
+    if (searchedTitle.length > 0) {
+      res.status(200).json({
+        success: true,
+        body: searchedTitle
+    })
+    } else {
+        res.status(404).json({
+          success: false,
+          body: {
+            message: "Movie title not found"
+          }
+      })
+    }
+  } catch(err) {
+      res.status(500).json({
+        success: false,
+        body: {
+          message: "Invalid movie title"
+        }
+      })
+  }
+});
+
+
+//Get all directors in the json
+app.get("/directors", async (req, res) => {
+  try {
+    const directorList = await A24Movies.find({}, { Directed_by: 1, _id: 0 });
+    const directorListExtracted = directorList.map(movie => movie.Directed_by).flat();
+
+    const directorObjects = directorListExtracted.map((director, index) => {
+      return {
+        id: index,
+        name: director
+      };
+    });
+
+    if (directorListExtracted) {
+      res.status(200).json({
+        success: true,
+        body: directorObjects
+      })
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: "List of directors not found"
+        }
+      })
+    }
+  } catch(err) {
+    res.status(500).json({
+      success: false,
+      body: {
+        message: "Invalid list of directors"
+      }
+    })
+  }
+  });
+
+  //Get movies by a specific director
+app.get("/directors/:name", async (req, res) => {
+  const singleDirector = req.params.name;
+  const directorRegex = new RegExp(singleDirector, 'i')
+  try {
+    const directorsMovies = await A24Movies.find({ Directed_by: { $regex: directorRegex } });
+    const directorObjects = directorsMovies.reduce((result, movie) => {
+      const directors = movie.Directed_by || [];
+      directors.forEach(director => {
+        if (!result[director]) {
+          result[director] = {
+            name: director,
+            directed_movies: []
+          };
+        }
+        result[director].directed_movies.push(movie);
+      });
+      return result;
+    }, {});
+
+    const directorArray = Object.values(directorObjects).map((director, index) => {
+      return {
+        id: index,
+        name: director.name,
+        directed_movies: director.directed_movies
+      };
+    });
+
+    if (directorsMovies.length > 0) {
+      res.status(200).json({
+        success: true,
+        body: directorArray
+      })
+    } else {
+      res.status(404).json({
+        success: false,
+        body: {
+          message: "No movies by director found"
+        }
+      })
+    }
+  } catch(err) {
+    res.status(500).json({
+      success: false,
+      body: {
+        message: "Invalid director"
+      }
+    })
   }
   });
 
